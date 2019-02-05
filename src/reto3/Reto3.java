adf69eaf1e0eb83942949730b99c9d4bb67bb092
ulises101199 <ulises101199@hotmail.com>
2019-02-05 08:50:35 -0600

    ordeamientos

--- src/reto3/Reto3.java
+++ src/reto3/Reto3.java
@@ -18,7 +18,11 @@ public class Reto3 {
    
     public static BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
+        
         int puntaje;
+        int [] valores = new int [3];
+       int temp; 
+        
        constructor personaje1 = new constructor ();
        mago personaje2 = new mago ();
        guerrero personaje3 = new guerrero ();
@@ -31,6 +35,25 @@ public class Reto3 {
        System.out.println("Indique el puntaje para cada constructor");
        puntaje = Integer.parseInt(entrada.readLine());
        personaje3.setPuntaje(puntaje);
+       
+       valores [0] = personaje1.getPuntaje();
+       valores [0] = personaje2.getPuntaje();
+       valores [0] = personaje3.getPuntaje();
+       
+       for (int r = 0; r < valores.length; r ++){
+           for (int j = r; j > 0; j--){
+               
+               temp = valores [j];
+               valores [j] = valores [r-1];
+               valores [ j -1] = temp;
+               
+           }
+           
+           
+           for (int y = 0; y < valores.length; y ++){
+               System.out.println(valores [y]);
+           }
+       }     
     }
     
 }
