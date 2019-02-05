af97afa6e2395ba6136757fec99ff06b6a19e97c
ulises101199 <ulises101199@hotmail.com>
2019-02-05 08:10:50 -0600

    version32

--- src/reto3/Reto3.java
+++ src/reto3/Reto3.java
@@ -5,17 +5,32 @@
  */
 package reto3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author AlumnoUTM
  */
 public class Reto3 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+   
+    public static BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        int puntaje;
+       constructor personaje1 = new constructor ();
+       mago personaje2 = new mago ();
+       guerrero personaje3 = new guerrero ();
+       System.out.println("Indique el puntaje para cada constructor");
+       puntaje = Integer.parseInt(entrada.readLine());
+       personaje1.setPuntaje(puntaje);
+       System.out.println("Indique el puntaje para cada constructor");
+       puntaje = Integer.parseInt(entrada.readLine());
+       personaje2.setPuntaje(puntaje);
+       System.out.println("Indique el puntaje para cada constructor");
+       puntaje = Integer.parseInt(entrada.readLine());
+       personaje3.setPuntaje(puntaje);
     }
     
 }
